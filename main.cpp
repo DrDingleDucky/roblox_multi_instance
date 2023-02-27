352d0726cf0f99358726baae3dd5201ffc23d4d3
George Davidson <gd240673@gmail.com>
2023-02-26 21:48:54 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <Windows.h>
 
-int main() {
+int main()
+{
     std::cout << "                                     ---| Multi Roblox Instance |---" << std::endl;
     std::cout << "WARNING: All Roblox instances need to be closed before running this program. Otherwise, it will not work. " << std::endl;
     std::cout << "" << std::endl;
