cefa4791476d11324afce4def9edcdd1c211b13b
George Davidson <gd240673@gmail.com>
2023-03-09 12:15:28 -0500

    cleaned code

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 
 int main()
 {
+    std::string command;
+
     std::cout << "                                     ---| Multi Roblox Instance |---" << std::endl;
     std::cout << "WARNING: All Roblox instances need to be closed before running this program. Otherwise, it will not work. " << std::endl;
     std::cout << "" << std::endl;
 
     CreateMutexA(NULL, TRUE, "ROBLOX_singletonMutex");
 
-    std::string command;
-
     std::cout << "say something to quit: -> ";
     std::cin >> command;
-
+    
     return 0;
 }
